b9de4c08b18494deb767d1287e2e4ca4133c5163
yujinago <138287741+yujinago@users.noreply.github.com>
2023-08-19 03:03:51 +0000

    add category-新規登録

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,15 +1,29 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @category = Category.new
+    @categories = Category.all
   end
 
   def create
-
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_categories_path
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
-
+    @category = Category.find(params[:id])
+    @category.update
+    redirect_to admin_categories_path
   end
+  
+  private
+  
+  def category_params
+    params.require(:Category).permit(:name)
+  end
+  
 end
