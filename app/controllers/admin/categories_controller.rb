be4da7f736a0551615c2cf75417ad8792dbdc90f
yujinago <138287741+yujinago@users.noreply.github.com>
2023-08-19 10:05:18 +0000

    finish-category機能&ページ

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,8 +6,12 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    @category.save
-    redirect_to admin_categories_path
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render "index"
+    end
   end
 
   def edit
@@ -16,14 +20,17 @@ class Admin::CategoriesController < ApplicationController
 
   def update
     @category = Category.find(params[:id])
-    @category.update
-    redirect_to admin_categories_path
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render "edit"
+    end
   end
   
   private
   
   def category_params
-    params.require(:Category).permit(:name)
+    params.require(:category).permit(:name)
   end
   
 end
