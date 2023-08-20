2edb5039d1f8aa19a14ef276e3e591c1b80c7cad
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-08-20 02:52:42 +0000

    add mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,12 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
@@ -15,4 +19,10 @@ class Public::CustomersController < ApplicationController
 
   def check
   end
+  
+   private
+
+   def customer_params
+     params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postcode,:address,:phone_number,:email)
+   end
 end
