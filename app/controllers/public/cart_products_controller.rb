9a8780f828b3044798a4a54be55cf6cf158170a5
EC2 Default User <ec2-user@ip-172-31-2-112.ap-northeast-1.compute.internal>
2023-08-21 09:17:33 +0000

    [first contact]

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -7,11 +7,13 @@ class Public::CartProductsController < ApplicationController
   end
 
   def destroy
-
+    @cart_product = Cart_product.find(params[:id])
+    @cart_product.destroy
+    redirect_to request.referer
   end
 
   def destroy_all
-
+    
   end
 
   def create
