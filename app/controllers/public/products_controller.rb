e41a44af6bdd88bb3fb1a6467432bae9494f49c5
yujinago <138287741+yujinago@users.noreply.github.com>
2023-08-21 02:57:17 +0000

    add-public-product-一覧ページ

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,12 @@
 class Public::ProductsController < ApplicationController
   def index
+    @categories = Category.all
+    @products = Product.page(params[:page])
   end
 
   def show
+    @categories = Category.all
+    @product = Product.find(params[:id])
+    @cart_product = CartProduct.new
   end
 end
